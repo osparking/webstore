5f03cfaf84caba16ec9fff74060660dd18e221f9
Park, JongBum <jbpark03@gmail.com>
2021-09-07 17:37:10 +0900

    chap 2. section 10 prac- domain class created.
    step i to step iv done.

---
+++ src/main/java/com/ezen/webstore/domain/Product.java
@@ -0,0 +1,129 @@
+package com.ezen.webstore.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@EqualsAndHashCode
+@NoArgsConstructor
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String productId;
+	private String name;
+	private BigDecimal unitPrice;
+	private String unitPriceStr;
+	private String description;
+	private String manufacturer;
+	private String category;
+	private long unitsInStock;
+	private long unitsInOrder;
+	private boolean discontinued;
+	private String condition;
+	
+	public Product(String productId, 
+			String name, 
+			BigDecimal unitPrice) {
+		this.productId = productId;
+		this.name = name;
+		this.setUnitPrice(unitPrice);
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUnitPriceStr() {
+		return unitPriceStr;
+	}
+
+	public void setUnitPriceStr(String unitPriceStr) {
+		this.unitPriceStr = unitPriceStr;
+	}
+	
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+		DecimalFormat df = new DecimalFormat("#,###");
+		this.unitPriceStr = df.format(unitPrice);
+	} 
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public long getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(long unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public long getUnitsInOrder() {
+		return unitsInOrder;
+	}
+
+	public void setUnitsInOrder(long unitsInOrder) {
+		this.unitsInOrder = unitsInOrder;
+	}
+
+	public boolean isDiscontinued() {
+		return discontinued;
+	}
+
+	public void setDiscontinued(boolean discontinued) {
+		this.discontinued = discontinued;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+}
