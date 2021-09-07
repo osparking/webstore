21370a32596eff816a651527e9a265e14912c683
Park, JongBum <jbpark03@gmail.com>
2021-09-07 14:43:33 +0900

    First SpringMVC project - welcome.

---
+++ src/main/java/com/ezen/webstore/config/WebApplicationContextConfig.java
@@ -0,0 +1,35 @@
+package com.ezen.webstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+//@formatter:off
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.ezen.webstore")
+public class WebApplicationContextConfig 
+		extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public InternalResourceViewResolver 
+			getInternalResourceViewResolver() {
+		var resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+	}
+}
