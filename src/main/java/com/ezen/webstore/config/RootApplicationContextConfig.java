5ff16bcfb452bf92a95ee26d75e708d31d2df3a1
Park, JongBum <jbpark03@gmail.com>
2021-09-08 10:44:21 +0900

    Persistence layer being done upto 2 sql files
    webstore database created.

---
+++ src/main/java/com/ezen/webstore/config/RootApplicationContextConfig.java
@@ -0,0 +1,30 @@
+package com.ezen.webstore.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("com.ezen.webstore")
+public class RootApplicationContextConfig {
+	// MariaDB 사용 때:
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName(org.mariadb.jdbc.Driver.class.getName());
+		ds.setUrl("jdbc:mariadb://localhost:3306/webstore");
+		ds.setUsername("myself");
+		ds.setPassword("1234");
+		return ds;
+	}
+	
+	@Bean
+	public NamedParameterJdbcTemplate getJdbcTemplate() {
+		return new NamedParameterJdbcTemplate(dataSource());
+	}
+}
+
