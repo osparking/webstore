bd8e6ea5d6f274b0b4c3034b3f3377fab09d03b3
Park, JongBum <jbpark03@gmail.com>
2021-09-09 11:16:59 +0900

    Lombok used in Customer domain class.

--- src/main/java/com/ezen/webstore/config/RootApplicationContextConfig.java
+++ src/main/java/com/ezen/webstore/config/RootApplicationContextConfig.java
@@ -3,14 +3,12 @@ package com.ezen.webstore.config;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class RootApplicationContextConfig {
-	// MariaDB 사용 때:
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource ds = new DriverManagerDataSource();
--- src/main/java/com/ezen/webstore/domain/Customer.java
+++ src/main/java/com/ezen/webstore/domain/Customer.java
@@ -1,55 +1,16 @@
 package com.ezen.webstore.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @EqualsAndHashCode
 public class Customer implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String customerId;
 	private String name;
 	private String address;
 	private int noOfOrdersMade;
-	public String getCustomerId() {
-		return customerId;
-	}
-	public void setCustomerId(String customerId) {
-		this.customerId = customerId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public int getNoOfOrdersMade() {
-		return noOfOrdersMade;
-	}
-	public void setNoOfOrdersMade(int noOfOrdersMade) {
-		this.noOfOrdersMade = noOfOrdersMade;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(address, customerId, name, noOfOrdersMade);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Customer other = (Customer) obj;
-		return Objects.equals(address, other.address) && Objects.equals(customerId, other.customerId)
-				&& Objects.equals(name, other.name) && noOfOrdersMade == other.noOfOrdersMade;
-	}
 }
