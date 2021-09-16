9d1a10b557f0397a298e1b8a42a19b4b12dbf26b
Park, JongBum <jbpark03@gmail.com>
2021-09-16 10:51:04 +0900

    static images displayed

--- src/main/java/com/ezen/webstore/config/WebApplicationContextConfig.java
+++ src/main/java/com/ezen/webstore/config/WebApplicationContextConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -17,6 +18,11 @@ import org.springframework.web.util.UrlPathHelper;
 @ComponentScan("com.ezen.webstore")
 public class WebApplicationContextConfig 
 		extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/**")
+			.addResourceLocations("/resources/images/");
+	}
 
 	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) {
